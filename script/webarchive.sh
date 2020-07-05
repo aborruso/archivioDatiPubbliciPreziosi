9ebeb60ab0e5ed97f0d1c296bfd1405394afcd35
Andrea Borruso <aborruso@gmail.com>
2020-07-05 16:33:47 +0200

    update

---
+++ script/webarchive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+<"$folder"/../risorse/listArchive.yml yq -r '.[].URL'>"$folder"/../risorse/listArchive.txt
+
+while read p; do
+  echo "$p"
+  curl -X POST -H "Accept: application/json"  -H "Authorization: LOW $SUPER_SECRET" -d 'url='"$p"'/&capture_outlinks=1&capture_screenshot=1&outlinks_availability=1'  https://web.archive.org/save
+  sleep 10;
+done <"$folder"/../risorse/listArchive.txt
