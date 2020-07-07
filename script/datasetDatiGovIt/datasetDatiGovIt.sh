b31ee49e66913ea4c182fcf1592e8fae003c8745
Andrea Borruso <aborruso@gmail.com>
2020-07-07 17:44:10 +0200

    aggiunto elenco dataset portale open data

---
+++ script/datasetDatiGovIt/datasetDatiGovIt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+nome="datasetDatiGovIt"
+
+# curl -kL "https://www.dati.gov.it/api/3/action/package_list" | jq -r '.result[]' | mlr --csv --implicit-csv-header sort -f 1 then label id
+
+URL="https://www.dati.gov.it/api/3/action/package_list"
+
+# leggi la risposta HTTP del sito
+code=$(curl -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
+
+# se il sito è raggiungibile scarica i dati
+if [ $code -eq 200 ]; then
+
+  curl -kL "https://www.dati.gov.it/api/3/action/package_list" | jq -r '.result[]' | mlr --csv --implicit-csv-header sort -f 1 then label id >"$folder"/../../docs/"$nome"/"$nome".csv
+
+fi
