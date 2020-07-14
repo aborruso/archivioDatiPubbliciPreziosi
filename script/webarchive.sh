7de81762eba45e2d2775bdae8bc28dca3c4c3642
Andrea Borruso <aborruso@gmail.com>
2020-07-14 08:09:25 +0200

    test

--- script/webarchive.sh
+++ script/webarchive.sh
@@ -9,6 +9,6 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rm "$folder"/webarchiveLatest.log
 while read p; do
   echo "$p"
-  curl -X POST -H "Accept: application/json"  -H "Authorization: LOW $SUPER_SECRET" -d 'url='"$p"'/&capture_outlinks=1&capture_screenshot=1&outlinks_availability=1'  https://web.archive.org/save >>"$folder"/webarchiveLatest.log
+  curl -X POST -H "Accept: application/json"  -H "Authorization: LOW $SUPER_SECRET" -d 'url='"$p"'&capture_outlinks=1&capture_screenshot=1&outlinks_availability=1'  https://web.archive.org/save >>"$folder"/webarchiveLatest.log
   sleep 10;
 done <"$folder"/../risorse/listArchive.txt
