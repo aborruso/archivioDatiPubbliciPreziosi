fd76a63b9de025ad4e1ce6fb2b86bbcbe01f3140
Andrea Borruso <aborruso@gmail.com>
2020-07-15 08:24:25 +0200

    close  #7

--- script/webarchive.sh
+++ script/webarchive.sh
@@ -4,11 +4,18 @@ set -x
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-<"$folder"/../risorse/listArchive.yml yq -r '.[].URL'>"$folder"/../risorse/listArchive.txt
+yq <"$folder"/../risorse/listArchive.yml -r '.[].URL' >"$folder"/../risorse/listArchive.txt
+yq <"$folder"/../risorse/listArchive.yml . | mlr --j2t cut -f URL,if_not_archived_within | tail -n +2 >"$folder"/../risorse/listArchive.tsv
 
 rm "$folder"/webarchiveLatest.log
-while read p; do
-  echo "$p"
-  curl -X POST -H "Accept: application/json"  -H "Authorization: LOW $SUPER_SECRET" -d 'url='"$p"'&capture_outlinks=1&capture_screenshot=1&outlinks_availability=1'  https://web.archive.org/save >>"$folder"/webarchiveLatest.log
-  sleep 10;
-done <"$folder"/../risorse/listArchive.txt
+#while read p; do
+#  echo "$p"
+#  curl -X POST -H "Accept: application/json"  -H "Authorization: LOW $SUPER_SECRET" -d 'url='"$p"'&capture_outlinks=1&capture_screenshot=1&outlinks_availability=1'  https://web.archive.org/save >>"$folder"/webarchiveLatest.log
+#  sleep 10;
+#done <"$folder"/../risorse/listArchive.txt
+
+while IFS=$'\t' read -r url time; do
+  echo "$time"
+  curl -X POST -H "Accept: application/json" -H "Authorization: LOW $SUPER_SECRET" -d 'url='"$p"'&capture_outlinks=1&capture_screenshot=1&outlinks_availability=1&if_not_archived_within='"$time"'' https://web.archive.org/save >>"$folder"/webarchiveLatest.log
+  sleep 10
+done <"$folder"/../risorse/listArchive.tsv
