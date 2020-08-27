588690b763551e312f5663aa6804dd8ef9a88be8
Andrea Borruso <aborruso@gmail.com>
2020-08-27 15:34:46 +0200

    aggiunti comuni subentrati

--- script/ANPRdatiMigrazione/ANPRdatiMigrazione.sh
+++ script/ANPRdatiMigrazione/ANPRdatiMigrazione.sh
@@ -20,6 +20,19 @@ if [ $code -eq 200 ]; then
 
 fi
 
+URLsubentrati="https://www.anpr.interno.it/portale/documents/20182/241820/Comuni+subentrati.xlsx"
+
+# leggi la risposta HTTP del sito
+code=$(curl -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
+
+# se il sito è raggiungibile scarica i dati
+if [ $code -eq 200 ]; then
+
+  curl -skL "$URLsubentrati" >"$folder"/../../docs/"$nome"/"$nome"_subentrati.xlsx
+  in2csv -I "$folder"/../../docs/"$nome"/"$nome"_subentrati.xlsx | mlr --csv remove-empty-columns then filter -S '${Codice Istat}=~".+"' >"$folder"/../../docs/"$nome"/"$nome"_subentrati.csv
+
+fi
+
 # estrai dati aggregati
 <"$folder"/../../docs/"$nome"/"$nome".geojson jq '.aggregates.aggr_by_provinces' | mlr --j2c unsparsify then sort -f provincia >"$folder"/../../docs/"$nome"/aggr_by_provinces.csv
 <"$folder"/../../docs/"$nome"/"$nome".geojson jq '.aggregates.aggr_by_regions' | mlr --j2c unsparsify then sort -f  regione>"$folder"/../../docs/"$nome"/aggr_by_regions.csv
