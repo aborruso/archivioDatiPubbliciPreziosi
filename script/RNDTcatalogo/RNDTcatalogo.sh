38beb62b4caec29ae7f992b557bed42a09910480
Andrea Borruso <aborruso@gmail.com>
2020-08-28 18:16:06 +0200

    update

--- script/RNDTcatalogo/RNDTcatalogo.sh
+++ script/RNDTcatalogo/RNDTcatalogo.sh
@@ -16,6 +16,6 @@ if [[ "$code" -eq 200 ]]; then
 
   ogr2ogr -F CSV "$folder"/../../docs/"$nome"/RNDTcatalogo.csv "CSW:http://geodati.gov.it/RNDT/csw" -oo ELEMENTSETNAME=full -oo FULL_EXTENT_RECORDS_AS_NON_SPATIAL=YES -oo MAX_RECORDS=500 --config GML_SKIP_CORRUPTED_FEATURES YES
   mlr -I --csv sort -r modified "$folder"/../../docs/"$nome"/RNDTcatalogo.csv
-  mlr --csv cut -f identifier then put -S '$identifier=sub($identifier,":.*","")' then count -g identifier then rename identifier,cod_amm then sort -nr count "$folder"/../../docs/"$nome"/RNDTcatalogo.csv >"$folder"/../../docs/"$nome"/IPA.csv
+  mlr --csv cut -f identifier then put -S '$identifier=sub($identifier,":.*","")' then count -g identifier then rename identifier,cod_amm then sort -f cod_amm then filter -S -x '$cod_amm=~"[{]"' "$folder"/../../docs/"$nome"/RNDTcatalogo.csv >"$folder"/../../docs/"$nome"/IPA.csv
 
 fi
