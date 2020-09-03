799173e8b4a2ae9162e648e54f27320bb60a6f38
Andrea Borruso <aborruso@gmail.com>
2020-09-03 19:37:15 +0200

    close creare script per generazione struttura cartelle e file di base #5

---
+++ script/buildStructure.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+yq <"$folder"/../risorse/list.yml -r '.[].nome' | while read line; do
+  # crea cartelle per dataset (in docs e script), se non esistono
+  mkdir -p "$folder"/../docs/"$line"
+  touch "$folder"/../docs/"$line"/.keep
+  mkdir -p "$folder"/../script/"$line"
+  touch "$folder"/../script/"$line"/.keep
+
+  # crea script dataset se non esiste
+  if [ ! -f "$folder"/../script/"$line"/"$line".sh ]; then
+    touch "$folder"/../script/"$line"/"$line".sh
+  fi
+
+  # crea workflow dataset se non esiste
+  if [ ! -f "$folder"/../.github/workflows/"$line".yml ]; then
+    touch "$folder"/../.github/workflows/"$line".yml
+    echo "non esiste"
+  fi
+done
