4656826890b38943cd5d1872d88ae081049c0a59
Andrea Borruso <aborruso@gmail.com>
2020-09-03 19:46:04 +0200

    inserisci in anagrafica soltanto i dataset che risultanto pronti

--- script/anagrafica.sh
+++ script/anagrafica.sh
@@ -7,3 +7,6 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 <"$folder"/../risorse/list.yml yq . | mlr --j2c unsparsify >"$folder"/../anagrafica.csv
 
 cp "$folder"/../anagrafica.csv "$folder"/../docs/anagrafica.csv
+
+# inserisci in anagrafica soltanto ciò che è pronto
+mlr -I --csv filter '$ready=="true"' "$folder"/../docs/anagrafica.csv
