defe1a5d9f146adab4f2f9326bd36dfa8fc0ae97
Andrea Borruso <aborruso@gmail.com>
2020-09-04 08:09:20 +0200

    update

--- script/anagrafica.sh
+++ script/anagrafica.sh
@@ -4,9 +4,7 @@ set -x
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-<"$folder"/../risorse/list.yml yq . | mlr --j2c unsparsify >"$folder"/../anagrafica.csv
-
-cp "$folder"/../anagrafica.csv "$folder"/../docs/anagrafica.csv
+<"$folder"/../risorse/list.yml yq . | mlr --j2c unsparsify >"$folder"/../docs/anagrafica.csv
 
 # inserisci in anagrafica soltanto ciò che è pronto
 mlr -I --csv filter '$ready=="true"' "$folder"/../docs/anagrafica.csv
