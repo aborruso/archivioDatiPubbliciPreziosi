144a0b2ab59c674626a2886504ad7415bf74e0f9
Andrea Borruso <aborruso@gmail.com>
2021-03-01 08:59:14 +0100

    aggiunta estrazione comuni IPA

--- script/indiceIPA/indiceIPA.sh
+++ script/indiceIPA/indiceIPA.sh
@@ -14,6 +14,9 @@ yq <"$folder"/../../risorse/list.yml -r '.[] | select(.nome=="'"$nome"'").URL' |
 # scarica risorse
 while IFS=$'\t' read -r url file; do
   curl -ksL "$url" >"$folder"/../../docs/"$nome"/"$file"
-  mlr -I --icsvlite --ifs "\t" --otsv  sort -f cod_amm "$folder"/../../docs/"$nome"/"$file"
+  mlr -I --icsvlite --ifs "\t" --otsv sort -f cod_amm "$folder"/../../docs/"$nome"/"$file"
 done <"$folder"/tmp.tsv
 
+if [ -f "$folder"/../../docs/"$nome"/amministrazioni.txt ]; then
+  mlr --tsv clean-whitespace then filter -S '(tolower($cod_amm)=~"^c_" || tolower($des_amm)=~"^comune") && ($tipologia_istat=~"^Comuni ") && (tolower($des_amm)!=~"^conso")' "$folder"/../../docs/"$nome"/amministrazioni.txt >"$folder"/../../docs/"$nome"/comuniIPA.csv
+fi
