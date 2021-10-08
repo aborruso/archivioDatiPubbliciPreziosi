a2d79ff0b9124eebda798bb35bc2cbda3137567a
Andrea Borruso <aborruso@gmail.com>
2021-10-08 19:35:40 +0200

    inseriti Enti da catalogo open data iPA

---
+++ script/indiceIPACatalogo/indiceIPACatalogo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+git pull
+
+nome="indiceIPACatalogo"
+
+mkdir -p "$folder"/../../docs/"$nome"
+
+# estrai URL file e nome file
+yq <"$folder"/../../risorse/list.yml -r '.[] | select(.nome=="'"$nome"'").URL' | mlr --j2t cat | tail -n +2 >"$folder"/tmp.tsv
+
+# scarica risorse
+while IFS=$'\t' read -r url file; do
+  curl -ksL "$url" >"$folder"/../../docs/"$nome"/"$file"
+  mlr -I --csv cat "$folder"/../../docs/"$nome"/"$file"
+done <"$folder"/tmp.tsv
+
