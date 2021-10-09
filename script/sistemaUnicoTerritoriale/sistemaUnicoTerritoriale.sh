16284423105a1f7caf4a8127eabb1b7fd4cbdb7b
Andrea Borruso <aborruso@gmail.com>
2021-10-09 19:11:06 +0200

    aggiunta condizione di applicazione comando solo file comuni

--- script/sistemaUnicoTerritoriale/sistemaUnicoTerritoriale.sh
+++ script/sistemaUnicoTerritoriale/sistemaUnicoTerritoriale.sh
@@ -18,7 +18,9 @@ yq <"$folder"/../../risorse/list.yml -r '.[] | select(.nome=="'"$nome"'").URL' |
 
 # scarica risorse
 while IFS=$'\t' read -r url file; do
-  curl -ksL "$url" -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'  >"$folder"/../../docs/"$nome"/raw_"$file"
-  mlr  --icsvlite --ocsv --ifs ";" put -S '${CODICE ISTAT}=regextract_or_else(${CODICE ISTAT},"[0-9]+","")' "$folder"/../../docs/"$nome"/raw_"$file" >"$folder"/../../docs/"$nome"/"$file"
+  curl -ksL "$url" -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36' >"$folder"/../../docs/"$nome"/raw_"$file"
+  # se il file è quello dei comuni, cambia il separatore da ";" a "," ed estrai codice comunale istat
+  if [[ $(echo "$file" | grep 'comuniSistemaUnicoTerritoriale') ]]; then
+    mlr --icsvlite --ocsv --ifs ";" put -S '${CODICE ISTAT}=regextract_or_else(${CODICE ISTAT},"[0-9]+","")' "$folder"/../../docs/"$nome"/raw_"$file" >"$folder"/../../docs/"$nome"/"$file"
+  fi
 done <"$folder"/tmp.tsv
-
