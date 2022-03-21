fa1207edaf26b3832559b0cfd50cae068d3c3c9c
Andrea Borruso <aborruso@gmail.com>
2022-03-21 21:16:23 +0100

    test perl

--- script/carburanteAngragraficaStazioni/carburanteAngragraficaStazioni.sh
+++ script/carburanteAngragraficaStazioni/carburanteAngragraficaStazioni.sh
@@ -19,6 +19,7 @@ if [ $code -eq 200 ]; then
   curl -skL "$URL" >"$folder"/../../docs/"$nome"/anagrafica_impianti_attivi.csv
   # versione cleaned
   # nota bene, vengono rimossi tutte le "
+  <"$folder"/../../docs/"$nome"/anagrafica_impianti_attivi.csv perl -n -mHTML::Entities -e ' ; print HTML::Entities::decode_entities($_) ;' >"$folder"/../../docs/"$nome"/test.csv
   <"$folder"/../../docs/"$nome"/anagrafica_impianti_attivi.csv sed -r 's/"//g' | tail -n +2 | grep -vP '&#' | mlr --csv --ifs ";" clean-whitespace then sort -n idImpianto >"$folder"/../../docs/"$nome"/anagrafica_impianti_attivi-cleaned.csv
 
   <"$folder"/../../docs/"$nome"/anagrafica_impianti_attivi.csv tail -n +2 | grep -vP '&#' >"$folder"/../../docs/"$nome"/tmp.csv
