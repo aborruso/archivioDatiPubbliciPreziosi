37e3eb136c41d79324a8be59b079d7f79c394597
Andrea Borruso <aborruso@gmail.com>
2023-06-27 17:58:39 +0100

    Update datasetDatiGovIt.sh

--- script/datasetDatiGovIt/datasetDatiGovIt.sh
+++ script/datasetDatiGovIt/datasetDatiGovIt.sh
@@ -10,7 +10,7 @@ nome="datasetDatiGovIt"
 
 # curl -kL "https://www.dati.gov.it/api/3/action/package_list" | jq -r '.result[]' | mlr --csv --implicit-csv-header sort -f 1 then label id
 
-URL="https://www.dati.gov.it/api/3/action/package_list"
+URL="https://www.dati.gov.it/opendata/api/3/action/package_list"
 
 # leggi la risposta HTTP del sito
 code=$(curl -k -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
@@ -18,6 +18,6 @@ code=$(curl -k -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
 # se il sito è raggiungibile scarica i dati
 if [ $code -eq 200 ]; then
 
-  curl -kL "https://www.dati.gov.it/api/3/action/package_list" | jq -r '.result[]' | mlr --csv --implicit-csv-header sort -f 1 then label id >"$folder"/../../docs/"$nome"/"$nome".csv
+  curl -kL "$URL" | jq -r '.result[]' | mlr --csv --implicit-csv-header sort -f 1 then label id >"$folder"/../../docs/"$nome"/"$nome".csv
 
 fi
