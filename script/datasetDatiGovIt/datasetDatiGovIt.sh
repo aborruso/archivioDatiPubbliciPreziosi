d4a6247b7550e7482e25b397cd1915b0108413be
aborruso <aborruso@gmail.com>
2023-07-08 17:16:43 +0200

    aggiunto URL org

--- script/datasetDatiGovIt/datasetDatiGovIt.sh
+++ script/datasetDatiGovIt/datasetDatiGovIt.sh
@@ -20,9 +20,16 @@ if [ $code -eq 200 ]; then
 
   curl -kL "$URL" | jq -r '.result[]' | mlr --csv --implicit-csv-header sort -f 1 then label id >"$folder"/../../docs/"$nome"/"$nome".csv
 
+  # scarica elenco organizzazioni
   ckanapi -r https://dati.gov.it/opendata/ dump organizations --all -p 4 -O "$folder"/../../docs/"$nome"/organizzazioni.jsonl
+
+  # ordina elenco organizzazioni per name
   <"$folder"/../../docs/"$nome"/organizzazioni.jsonl jq -cs 'sort_by(.name)|.[]' >"$folder"/../../docs/"$nome"/organizzazioni.jsonl.tmp
   mv "$folder"/../../docs/"$nome"/organizzazioni.jsonl.tmp "$folder"/../../docs/"$nome"/organizzazioni.jsonl
-  <"$folder"/../../docs/"$nome"/organizzazioni.jsonl mlr --j2c cut -f name,display_name,package_count,created,id,identifier then sort -f name >"$folder"/../../docs/"$nome"/organizzazioni-info.csv
+
+  # estrai CSV con alcune info su organizzazioni
+  <"$folder"/../../docs/"$nome"/organizzazioni.jsonl mlr --j2c cut -f name,display_name,package_count,created,id,identifier then sort -f name then put '$URL="https://dati.gov.it/view-dataset?organization=".$name' >"$folder"/../../docs/"$nome"/organizzazioni-info.csv
+
+  # estrai soltanto nomi organizzazioni
   <"$folder"/../../docs/"$nome"/organizzazioni.jsonl mlr --j2c cut -f name then sort -f name >"$folder"/../../docs/"$nome"/organizzazioni-name.csv
 fi
