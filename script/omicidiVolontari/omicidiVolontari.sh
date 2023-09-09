2302e275d2ec2d14286a430b90b773547d3a2003
aborruso <aborruso@gmail.com>
2023-09-09 18:16:17 +0200

    test tor

--- script/omicidiVolontari/omicidiVolontari.sh
+++ script/omicidiVolontari/omicidiVolontari.sh
@@ -19,18 +19,18 @@ radice="https://www.interno.gov.it"
 
 
 # leggi la risposta HTTP del sito
-code=$(curl -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
+code=$(curl --socks5-hostname localhost:9050 -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
 
 # se il sito è raggiungibile scarica i dati
 if [ $code -eq 200 ]; then
-  curl  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' "$URL" --compressed | \
+  curl --socks5-hostname localhost:9050 -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' "$URL" --compressed | \
   scrape -be '//a[contains(@href, ".csv")]' | xq '.html.body.a' | mlr --json unsparsify > "$folder"/tmp/tmp.json
 
   # estrai i link ai file csv
   while read line; do
     link=$(echo "$line" | jq -r '."@href"')
     echo "$link"
-    curl -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' "$radice$link" | \
+    curl --socks5-hostname localhost:9050 -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' "$radice$link" | \
     mlrgo --csv --ifs ";" --implicit-csv-header remove-empty-columns then skip-trivial-records then clean-whitespace | \
     mlrgo --csv remove-empty-columns then skip-trivial-records then clean-whitespace | \
     tail -n +3 |\
